59eb305a7d73e8b9d8bc055f9fe8412f7ace92c3
losnau <losbyday@163.com>
2018-02-27 20:57:44 +0800

    Update oa_opuser_selfsign.py

--- SJS_Project/OA/oa_opuser_selfsign.py
+++ SJS_Project/OA/oa_opuser_selfsign.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 #一个OA，admin用户快速填写我的任务，非自动
+#注意：代码从python2.7 人工改成python3，没有经过测试过，修改部分有print函数和raw_input函数
 '''
 可以优化部分
 把代码细化，将很多共有部分写成小函数，快速调用，防止写重复代码
@@ -65,7 +66,7 @@ class OA_Admin:
     def work(self):
         allworks_url=self.handle_allworks()
         for url in allworks_url:
-            print '当前工单的地址：'+url
+            print('当前工单的地址：'+url) 
             work_response=self.s.get(url)
             ##获取每个表单信息的正则表达式
             urlpattern = re.compile(
@@ -84,10 +85,10 @@ class OA_Admin:
             liuzhuan_url_items = re.findall(liuzhuan_url_pattern, work_response.content)
             for  liuzhuan_url_item in  liuzhuan_url_items:
                 #打印流转页面
-                print '打印流转页面：'+self.begin_url+liuzhuan_url_item
+                print('打印流转页面：'+self.begin_url+liuzhuan_url_item) 
                 liuzhuan_response=self.s.get(self.begin_url+liuzhuan_url_item)
-                # print liuzhuan_response.content
-                # print liuzhuan_response.request.headers
+                # print(liuzhuan_response.content) 
+                # print(liuzhuan_response.request.headers) 
                 # exit(9)
             ##确定这个任务是否与工单相关联
             urlpattern = re.compile('工单编号：(.*?)）', re.S)
@@ -95,33 +96,33 @@ class OA_Admin:
             n=0
             for gdhao_item in gdhao_items:
                 if gdhao_item:
-                    print '跟工单相关联,工单号为：', gdhao_item
+                    print('跟工单相关联,工单号为：', gdhao_item) 
                     liuzhuan_pattern= re.compile('技术开发组评审.*?<td>.*?<td>.*?<td>(.*?)</td>',re.S)
                 else:
                     print '没有跟工单号相关联'
                     liuzhuan_pattern = re.compile('IT部负责人审批.*?<td>.*?<td>.*?<td>(.*?)</td>', re.S)
                     n=n+1
                 liuzhuan_items = re.findall(liuzhuan_pattern, liuzhuan_response.content)
-                print '最后时间：',
-                print liuzhuan_items
+                print('最后时间：',) 
+                print(liuzhuan_items) 
             self.update_date['realInDate']=raw_input('第一次时间：')
             self.update_date['realOutDate'] = raw_input('第二次时间：')
-            print '第一次时间：'+ self.update_date['realInDate']
-            print '第二次时间：'+self.update_date['realOutDate']
+            print('第一次时间：'+ self.update_date['realInDate']) 
+            print('第二次时间：'+self.update_date['realOutDate']) 
             #验证提交信息
             # print self.update_date
-            conti=raw_input('\n是否提交？输入任意数字表示退出，直接回车表示提交：')
+            conti=input('\n是否提交？输入任意数字表示退出，直接回车表示提交：')
             if not conti:
-                print '为空，将会提交。'
+                print('为空，将会提交。') 
             else:
-                print '程序退出'
+                print('程序退出') 
                 exit(9)
             tijiao_response = self.s.post(self.tijiao_url,data=self.update_date)
             print tijiao_response.status_code
             self.num=self.num-1
-            #print tijiao_response.content
-            print
-            print '--------下一个--------'+'还有'+str(self.num)+'-----------'
+            #print(tijiao_response.content) 
+            print('')
+            print('--------下一个--------'+'还有'+str(self.num)+'-----------') 
 
 admin=OA_Admin()
 admin.work()
