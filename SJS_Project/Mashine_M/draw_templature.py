83423c93a12f27444b77bdca629d73748f2139d1
losnau <losbyday@163.com>
2018-02-27 20:36:59 +0800

    Update draw_templature.py

--- SJS_Project/Mashine_M/draw_templature.py
+++ SJS_Project/Mashine_M/draw_templature.py
@@ -2,6 +2,7 @@
 import  csv
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+#解决图片X、Y轴标题乱码
 mpl.rcParams['font.sans-serif'] = ['SimHei'];
 mpl.rcParams['axes.unicode_minus'] = False
 y1=[]
@@ -20,16 +21,16 @@ with open("temprature.csv", "rt", encoding="gbk") as vsvfile:
             y2.append(row[4])
             x1.append(row[0].replace('2018/','')+' '+row[1])
             temp = temp + 1
+#不显示第一行
 del y1[0]
 del y2[0]
 del x1[0]
-print(y1)
-print(y2)
-print(x1)
+
 x=range(len(x1))
 plt.figure(figsize=(20,15)) #创建绘图对象
 plt.subplot(211)    #这个必须放在plt.plo前面
 plt.plot(x, y1, 'ro--',label="送风温度", color="green", linewidth=1)   #在当前绘图对象绘图（X轴，Y轴，蓝色虚线，线宽度）
+#替换X轴，用定义的文本显示，X1为文本内容
 plt.xticks(x,x1,rotation = 90)
 plt.xlabel(u"日期时间") #X轴标签
 plt.ylabel(u"送风温度")  #Y轴标签
